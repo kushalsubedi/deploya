429b2f9101ffbe0d1d5f0de6589d6ce0108eacf0
kushalsubedi <kushalsubedi55@gmail.com>
2026-03-19 21:54:01 +0545

    ci test

--- cmd/add.go
+++ cmd/add.go
@@ -3,6 +3,7 @@ package cmd
 import "fmt"
 
 func runAdd(args []string) error {
+	// give user option to add various jobs and steps in their CI
 	fmt.Println("add: not yet implemented")
 	return nil
 }
